4a073b76567338aeac14b23a9dc7b960e2eb24e9
Igor <User@WIN-DJS489S31E1>
2020-10-26 12:32:25 +0200

    Added "Transaction" model, configured DB connection and first migration, created DB from scratch.

---
+++ TransactionReader/Data/TransationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TransactionReader.Models;
+
+namespace TransactionReader.Data
+{
+    public class TransationContext : DbContext
+    {
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public TransationContext() { }
+
+        public TransationContext(DbContextOptions<TransationContext> options)
+            : base(options) { }
+    }
+}
--- TransactionReader/Startup.cs
+++ TransactionReader/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TransactionReader.Data;
 
 namespace TransactionReader
 {
@@ -23,6 +25,10 @@ namespace TransactionReader
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configuring a DbContext and connection to DB
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<TransationContext>(options => options.UseSqlServer(connectionString));
+
             services.AddRazorPages();
         }
 
